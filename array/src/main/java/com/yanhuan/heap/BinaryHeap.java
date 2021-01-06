f4e51c933fe528b3580072341b44df5cb6027283
yanhuan <yanhuan6252@163.com>
2021-01-06 23:57:52 +0800

    docs：二叉堆实现添加注释

--- array/src/main/java/com/yanhuan/heap/BinaryHeap.java
+++ array/src/main/java/com/yanhuan/heap/BinaryHeap.java
@@ -10,12 +10,25 @@ import java.util.NoSuchElementException;
  * @date 2021-01-06 22:29
  */
 public class BinaryHeap {
+    /**
+     * d=2  表示二叉堆
+     */
     private static final int d = 2;
+
+    /**
+     * 数组实现
+     */
     private int[] heap;
+
+    /**
+     * 堆的大小
+     */
     private int heapSize;
 
-    /*** This will initialize our heap with default size.*/
-
+    /**
+     * 初始化
+     * This will initialize our heap with default size.
+     */
     public BinaryHeap(int capacity) {
         heapSize = 0;
         heap = new int[capacity + 1];
@@ -30,15 +43,29 @@ public class BinaryHeap {
         return heapSize == heap.length;
     }
 
+    /**
+     * 父节点索引
+     *
+     * @param i 当前节点
+     * @return 父节点索引
+     */
     private int parent(int i) {
         return (i - 1) / d;
     }
 
+    /**
+     * 子节点索引
+     *
+     * @param i 父节点
+     * @param k 子节点位置
+     * @return 索引值
+     */
     private int kthChild(int i, int k) {
         return d * i + k;
     }
 
     /**
+     * 插入元素
      * Inserts new element in to heap
      * Complexity: O(log N)
      * As worst case scenario, we need to traverse till the root
@@ -47,21 +74,29 @@ public class BinaryHeap {
         if (isFull()) {
             throw new NoSuchElementException("Heap is full, No space to insert new element");
         }
+        //新元素插入到堆尾
         heap[heapSize] = x;
         heapSize++;
+        //向上调整位置
         heapifyUp(heapSize - 1);
     }
 
-    /*** Deletes element at index x
+    /**
+     * 删除某个索引位置元素  （例如堆顶）
+     * Deletes element at index x
      * Complexity: O(log N)
      */
     public int delete(int x) {
         if (isEmpty()) {
             throw new NoSuchElementException("Heap is empty, No element to delete");
         }
+        //取出堆顶元素
         int maxElement = heap[x];
+        //将堆尾元素放置到堆顶
         heap[x] = heap[heapSize - 1];
+        //容量减一
         heapSize--;
+        //将置换上来的元素向下调整
         heapifyDown(x);
         return maxElement;
     }
@@ -69,11 +104,16 @@ public class BinaryHeap {
     /*** Maintains the heap property while inserting an element.*/
 
     private void heapifyUp(int i) {
+        //新增元素的值
         int insertValue = heap[i];
+        //如果新增元素的值大于父节点的值
         while (i > 0 && insertValue > heap[parent(i)]) {
+            //将父节点的值赋值在当前新元素的位置，等于父节点下移
             heap[i] = heap[parent(i)];
+            //当前节点索引改为父节点的索引
             i = parent(i);
         }
+        //当前节点值改为新增的值
         heap[i] = insertValue;
     }
 
@@ -81,26 +121,40 @@ public class BinaryHeap {
 
     private void heapifyDown(int i) {
         int child;
+        //当前节点值赋给临时变量
         int temp = heap[i];
+        //只要子节点索引小于总容量就循环--》子节点的最大索引不可能大于总容量
         while (kthChild(i, 1) < heapSize) {
             child = maxChild(i);
+            //当前节点值大于等于所有子节点值 退出循环
             if (temp >= heap[child]) {
                 break;
             }
+            //子节点的值赋给当前节点
             heap[i] = heap[child];
+            //当前节点下移
             i = child;
         }
+        //最初的值赋给当前节点
         heap[i] = temp;
     }
 
+    /**
+     * 找出当前节点的子节点中较大的那个节点
+     *
+     * @param i 父节点索引
+     * @return 子节点索引
+     */
     private int maxChild(int i) {
         int leftChild = kthChild(i, 1);
         int rightChild = kthChild(i, 2);
         return heap[leftChild] > heap[rightChild] ? leftChild : rightChild;
     }
 
-    /*** Prints all elements of the heap*/
-
+    /**
+     * 打印堆中所有的元素
+     * Prints all elements of the heap
+     */
     public void printHeap() {
         System.out.print("nHeap = ");
         for (int i = 0; i < heapSize; i++) {
@@ -110,6 +164,7 @@ public class BinaryHeap {
     }
 
     /**
+     * 找出堆中的最大值-->即堆顶元素  数组中的第一个值
      * This method returns the max element of the heap
      * complexity: O(1)
      */
